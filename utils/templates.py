bd863349a6b5d8f61c8cc04eeab323b1ba63d825
Олег <74383033+Oleg513@users.noreply.github.com>
2024-10-29 17:06:34 +0200

    Update templates.py

---
+++ utils/templates.py
@@ -0,0 +1,10 @@
+# utils/templates.py
+CHARACTER_DETAILS_TEMPLATE = (
+    "📖 **Деталі про героя {name}:**\n\n"
+    "<b>Клас:</b> {class}\n"
+    "<b>Роль:</b> {role}\n"
+    "<b>HP:</b> {hp}\n"
+    "<b>Атака:</b> {attack}\n"
+    "<b>Захист:</b> {defense}\n\n"
+    "🔗 Детальніше: {details_link}"
+)
