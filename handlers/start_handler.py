f479ae712cd0e8a290a89b28a526e07097780942
Олег <74383033+Oleg513@users.noreply.github.com>
2024-10-29 16:06:44 +0200

    Update start_handler.py

--- handlers/start_handler.py
+++ handlers/start_handler.py
@@ -1,6 +1,22 @@
-from telegram import Update
-from telegram.ext import CallbackContext
-from handlers.start_handler import start
+# handlers/start_handler.py
 
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Привіт! Я ваш бот для Mobile Legends. Як можу допомогти?')
+from telegram import Update, ReplyKeyboardMarkup, KeyboardButton
+from telegram.ext import ContextTypes, CommandHandler
+from config.settings import TELEGRAM_BOT_TOKEN
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    # Створення кнопок меню
+    buttons = [
+        [KeyboardButton("🧙‍♂️ Персонажі"), KeyboardButton("📚 Гайди"), KeyboardButton("🏆 Турніри")],
+        [KeyboardButton("🔄 Оновлення"), KeyboardButton("🆓 Початківець"), KeyboardButton("🔍 Пошук")],
+        [KeyboardButton("📰 Новини"), KeyboardButton("💡 Допомога"), KeyboardButton("🎮 Вікторини")]
+    ]
+    
+    # Створення клавіатури з кнопками
+    reply_markup = ReplyKeyboardMarkup(buttons, resize_keyboard=True)
+
+    # Відправка повідомлення користувачу з клавіатурою
+    await update.message.reply_text("🔍 Оберіть опцію:", reply_markup=reply_markup)
+
+# Створення обробника команди /start
+start_handler = CommandHandler("start", start)
