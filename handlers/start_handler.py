28d1a14028b8537e51fd5effff6eddc71e3dff90
Олег <74383033+Oleg513@users.noreply.github.com>
2024-10-29 17:00:16 +0200

    Update start_handler.py

--- handlers/start_handler.py
+++ handlers/start_handler.py
@@ -1,22 +1,17 @@
 # handlers/start_handler.py
-
 from telegram import Update, ReplyKeyboardMarkup, KeyboardButton
-from telegram.ext import ContextTypes, CommandHandler
-from config.settings import TELEGRAM_BOT_TOKEN
+from telegram.ext import ContextTypes
+from handlers.main_menu import main_menu_handler
+from handlers import States
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    # Створення кнопок меню
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    user_id = update.effective_user.id
+    context.bot_data['last_message_time'][user_id] = context.application.loop.time()
     buttons = [
         [KeyboardButton("🧙‍♂️ Персонажі"), KeyboardButton("📚 Гайди"), KeyboardButton("🏆 Турніри")],
         [KeyboardButton("🔄 Оновлення"), KeyboardButton("🆓 Початківець"), KeyboardButton("🔍 Пошук")],
         [KeyboardButton("📰 Новини"), KeyboardButton("💡 Допомога"), KeyboardButton("🎮 Вікторини")]
     ]
-    
-    # Створення клавіатури з кнопками
-    reply_markup = ReplyKeyboardMarkup(buttons, resize_keyboard=True)
-
-    # Відправка повідомлення користувачу з клавіатурою
+    reply_markup = ReplyKeyboardMarkup(buttons, resize_keyboard=True, one_time_keyboard=False)
     await update.message.reply_text("🔍 Оберіть опцію:", reply_markup=reply_markup)
-
-# Створення обробника команди /start
-start_handler = CommandHandler("start", start)
+    return States.MAIN_MENU
