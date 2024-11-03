f111f5eda04465e0308bf5231a00dbf4ff9a5522
Олег <74383033+Oleg513@users.noreply.github.com>
2024-11-03 23:30:21 +0200

    Update start_handler.py

--- handlers/start_handler.py
+++ handlers/start_handler.py
@@ -1,23 +1,26 @@
 # handlers/start_handler.py
-
 from telegram import Update, ReplyKeyboardMarkup, KeyboardButton
 from telegram.ext import ContextTypes
-from handlers import States
+from handlers.states import States
 import asyncio
 import logging
 
 logger = logging.getLogger(__name__)
+
+def get_main_menu_keyboard():
+    buttons = [
+        [KeyboardButton("🦸 Герої"), KeyboardButton("📚 Гайди"), KeyboardButton("🏆 Турніри")],
+        [KeyboardButton("🔄 Оновлення"), KeyboardButton("🆓 Початківець"), KeyboardButton("🔍 Пошук")],
+        [KeyboardButton("📰 Новини"), KeyboardButton("💡 Допомога"), KeyboardButton("🎮 Вікторини")]
+    ]
+    return ReplyKeyboardMarkup(buttons, resize_keyboard=True)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     user_id = update.effective_user.id
     current_time = asyncio.get_running_loop().time()
     context.bot_data['last_message_time'][user_id] = current_time
 
-    buttons = [
-        [KeyboardButton("🧙‍♂️ Персонажі"), KeyboardButton("📚 Гайди"), KeyboardButton("🏆 Турніри")],
-        [KeyboardButton("🔄 Оновлення"), KeyboardButton("🆓 Початківець"), KeyboardButton("🔍 Пошук")],
-        [KeyboardButton("📰 Новини"), KeyboardButton("💡 Допомога"), KeyboardButton("🎮 Вікторини")]
-    ]
-    reply_markup = ReplyKeyboardMarkup(buttons, resize_keyboard=True)
+    reply_markup = get_main_menu_keyboard()
     await update.message.reply_text("👋 Вітаю! Оберіть опцію з меню:", reply_markup=reply_markup)
     return States.MAIN_MENU
+    
