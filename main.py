bbbd53f2f69cc8336be7b22d0c5cad980f04899a
Олег <74383033+Oleg513@users.noreply.github.com>
2024-11-04 16:31:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 # main.py
 import logging
-import asyncio
-from telegram import Update
 from telegram.ext import (
     ApplicationBuilder,
     CommandHandler,
@@ -16,37 +14,14 @@ from handlers.start_handler import start
 from handlers.main_menu import main_menu_handler
 from handlers.characters import (
     handle_characters_menu,
-    handle_comparison_first_hero,
-    handle_comparison_second_hero,
     handle_selecting_hero_class,
     handle_selecting_hero,
+    handle_hero_functions_menu,
+    handle_comparison_first_hero,
+    handle_comparison_second_hero,
     handle_selecting_counter_hero
 )
-from handlers.guides import handle_guides_menu
-from handlers.tournaments import handle_tournaments_menu
-from handlers.updates import handle_updates_menu
-from handlers.beginner import handle_beginner_menu
-from handlers.news import handle_news_menu
-from handlers.help_menu import handle_help_menu
-from handlers.quizzes import (
-    handle_quizzes_menu,
-    handle_guess_the_hero
-)
-from handlers.search import (
-    handle_search_menu,
-    handle_search_performing,
-    handle_search_hero_guides
-)
-from handlers.comparisons import handle_comparisons_menu
-from handlers.emblems import handle_emblems_menu
-from handlers.items import handle_items_menu
-from handlers.recommendations import handle_recommendations
 from handlers.trigger_handler import trigger_handler
-
-# Визначення get_chat_id, якщо він ще не визначений
-async def get_chat_id(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    chat_id = update.effective_chat.id
-    await update.message.reply_text(f"Ваш Chat ID: {chat_id}")
 
 # Налаштування логування
 logging.basicConfig(
@@ -69,19 +44,11 @@ def main():
         states={
             States.MAIN_MENU: [MessageHandler(filters.TEXT & ~filters.COMMAND, main_menu_handler)],
             States.CHARACTERS_MENU: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_characters_menu)],
-            States.GUIDES_MENU: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_guides_menu)],
-            States.TOURNAMENTS_MENU: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_tournaments_menu)],
-            States.UPDATES_MENU: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_updates_menu)],
-            States.BEGINNER_MENU: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_beginner_menu)],
-            States.NEWS_MENU: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_news_menu)],
-            States.HELP_MENU: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_help_menu)],
-            States.QUIZZES_MENU: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_quizzes_menu)],
-            States.SEARCH_PERFORMING: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_search_performing)],
-            States.SEARCH_HERO_GUIDES: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_search_hero_guides)],
-            States.COMPARISON_FIRST_HERO: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_comparison_first_hero)],
-            States.COMPARISON_SECOND_HERO: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_comparison_second_hero)],
             States.SELECTING_HERO_CLASS: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_selecting_hero_class)],
             States.SELECTING_HERO: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_selecting_hero)],
+            States.HERO_FUNCTIONS_MENU: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_hero_functions_menu)],
+            States.COMPARISON_FIRST_HERO: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_comparison_first_hero)],
+            States.COMPARISON_SECOND_HERO: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_comparison_second_hero)],
             States.SELECTING_COUNTER_HERO: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_selecting_counter_hero)],
             # Додайте інші стани та обробники за потребою
         },
@@ -92,18 +59,9 @@ def main():
     )
     application.add_handler(conv_handler)
 
-    # Додаємо обробник для команди /get_chat_id
-    application.add_handler(CommandHandler("get_chat_id", get_chat_id))
-
-    # Додаємо обробник помилок
-    async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE):
-        logger.error(msg="Виникла помилка:", exc_info=context.error)
-    application.add_error_handler(error_handler)
-
+    # Запуск бота
     logger.info("🔄 Бот запущено.")
     application.run_polling()
 
-# Запуск бота
 if __name__ == '__main__':
     main()
-    
