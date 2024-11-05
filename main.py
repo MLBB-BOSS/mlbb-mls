17a2190167a9333d870dba41380f437a280db83e
Олег <74383033+Oleg513@users.noreply.github.com>
2024-11-05 14:08:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-# main.py
 import logging
+import os
 from telegram.ext import (
     ApplicationBuilder,
     CommandHandler,
@@ -7,9 +7,7 @@ from telegram.ext import (
     ConversationHandler,
     filters
 )
-from config.settings import TELEGRAM_BOT_TOKEN
 from handlers.states import States
-from handlers.start_handler import start
 from handlers.main_menu import main_menu_handler
 from handlers.characters import (
     handle_selecting_hero_class,
@@ -17,16 +15,20 @@ from handlers.characters import (
     handle_hero_functions_menu
 )
 from handlers.profile import profile_handler, profile_menu_handler
-from handlers.search import handle_search_menu, handle_search_performing, handle_search_hero_guides
 
 # Налаштування логування
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',  
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 logger = logging.getLogger(__name__)
 
 def main():
+    TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
+    if not TELEGRAM_BOT_TOKEN:
+        logger.error("Будь ласка, встановіть TELEGRAM_BOT_TOKEN як змінну середовища.")
+        return
+
     application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
 
     # Завантаження даних героїв та збереження у bot_data
@@ -37,6 +39,7 @@ def main():
     application.bot_data['heroes_data'] = heroes_data
 
     # Додаємо обробник команди /start
+    from handlers.start_handler import start
     application.add_handler(CommandHandler('start', start))
 
     # Додаємо обробник розмови
@@ -57,12 +60,6 @@ def main():
             ],
             States.PROFILE_MENU: [
                 MessageHandler(filters.TEXT & ~filters.COMMAND, profile_menu_handler)
-            ],
-            States.SEARCH_PERFORMING: [
-                MessageHandler(filters.TEXT & ~filters.COMMAND, handle_search_performing)
-            ],
-            States.SEARCH_HERO_GUIDES: [
-                MessageHandler(filters.TEXT & ~filters.COMMAND, handle_search_hero_guides)
             ],
             # Додайте інші стани за потреби
         },
