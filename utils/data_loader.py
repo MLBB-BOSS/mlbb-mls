7c9a5b2c6cef305d648518eb2411d65ca8ab5764
Олег <74383033+Oleg513@users.noreply.github.com>
2024-11-05 06:37:46 +0200

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -26,8 +26,8 @@ def load_hero_data(class_name, hero_name):
     """
     Завантажує дані про конкретного героя з відповідної папки.
     """
-    file_name = f"{hero_name.lower().replace(' ', '_')}.json"
-    file_path = os.path.join('data', 'heroes', class_name, file_name)
+    file_name = f"{hero_name.replace(' ', '')}.json"
+    file_path = os.path.join('data', 'heroes', class_name, hero_name, file_name)
     return load_json_data(file_path)
 
 def load_all_heroes():
@@ -40,11 +40,14 @@ def load_all_heroes():
     for class_dir in os.listdir(heroes_dir):
         class_path = os.path.join(heroes_dir, class_dir)
         if os.path.isdir(class_path):
-            all_heroes[class_dir] = []
-            for hero_file in os.listdir(class_path):
-                if hero_file.endswith('.json'):
-                    hero_data = load_json_data(os.path.join(class_path, hero_file))
+            class_name = class_dir
+            all_heroes[class_name] = []
+            for hero_dir in os.listdir(class_path):
+                hero_path = os.path.join(class_path, hero_dir)
+                if os.path.isdir(hero_path):
+                    hero_file = os.path.join(hero_path, f"{hero_dir}.json")
+                    hero_data = load_json_data(hero_file)
                     if hero_data:
-                        all_heroes[class_dir].append(hero_data)
+                        all_heroes[class_name].append(hero_data)
 
     return all_heroes
