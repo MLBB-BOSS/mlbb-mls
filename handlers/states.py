97c2b6343587dff534455a7d1a326be065e08be6
Олег <74383033+Oleg513@users.noreply.github.com>
2024-11-05 07:51:04 +0200

    Update states.py

--- handlers/states.py
+++ handlers/states.py
@@ -3,12 +3,7 @@ from enum import Enum, auto
 
 class States(Enum):
     MAIN_MENU = auto()
-    CHARACTERS_MENU = auto()
     SELECTING_HERO_CLASS = auto()
     SELECTING_HERO = auto()
     HERO_FUNCTIONS_MENU = auto()
-    COMPARISON_FIRST_HERO = auto()
-    COMPARISON_SECOND_HERO = auto()
-    SELECTING_COUNTER_HERO = auto()
-    # Видаляємо стани, для яких немає обробників
     # Додайте інші стани за потребою
