ce1793f3be0aa724d319a8ce03be713845d53a1d
Олег <74383033+Oleg513@users.noreply.github.com>
2024-11-05 11:01:31 +0200

    Update states.py

--- handlers/states.py
+++ handlers/states.py
@@ -5,15 +5,16 @@ class States:
     SELECTING_HERO_CLASS = 1
     SELECTING_HERO = 2
     HERO_FUNCTIONS_MENU = 3
-    GUIDES = 4
-    BUILDS = 5
-    NEWS = 6
-    EVENTS = 7
-    QUIZZES = 8
-    QUIZ_IN_PROGRESS = 9
+    STATISTICS = 4
+    GUIDES = 5
+    BUILDS = 6
+    NEWS = 7
+    EVENTS = 8
+    QUIZZES = 9
     ACHIEVEMENTS = 10
     COMMUNITY = 11
     POLLS = 12
     PROFILE = 13
     HELP = 14
+    GUIDES_MENU = 15
     # Додайте інші стани за потреби
