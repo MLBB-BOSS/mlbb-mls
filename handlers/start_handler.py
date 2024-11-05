59b706ed93686f90395ea3c08926f1c5eafdb789
Олег <74383033+Oleg513@users.noreply.github.com>
2024-11-05 16:48:54 +0200

    Update start_handler.py

--- handlers/start_handler.py
+++ handlers/start_handler.py
@@ -1,3 +1,4 @@
+# handlers/start_handler.py
 from telegram import Update
 from telegram.ext import ContextTypes
 from handlers.main_menu import get_main_menu_keyboard
@@ -5,5 +6,5 @@ from handlers.states import States
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     reply_markup = get_main_menu_keyboard()
-    await update.message.reply_text("Вітаю! Оберіть опцію з меню:", reply_markup=reply_markup)
+    await update.message.reply_text("Вітаємо! Оберіть опцію з меню:", reply_markup=reply_markup)
     return States.MAIN_MENU
