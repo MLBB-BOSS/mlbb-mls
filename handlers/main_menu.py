ab54e862fe395ad18da2982996eda72934942ff3
Олег <74383033+Oleg513@users.noreply.github.com>
2024-11-05 16:43:42 +0200

    Update main_menu.py

--- handlers/main_menu.py
+++ handlers/main_menu.py
@@ -1,8 +1,10 @@
+# handlers/main_menu.py
 from telegram import Update, ReplyKeyboardMarkup, KeyboardButton
 from telegram.ext import ContextTypes
 from handlers.states import States
 from handlers.characters import get_hero_classes_keyboard
 from handlers.profile import profile_handler
+from handlers.tier_list import send_tier_list  # Імпортуємо нову функцію
 import logging
 
 logger = logging.getLogger(__name__)
@@ -11,7 +13,7 @@ def get_main_menu_keyboard():
     buttons = [
         [KeyboardButton("🦸 Герої"), KeyboardButton("📖 Гайди"), KeyboardButton("🛠 Збірки")],
         [KeyboardButton("📰 Новини"), KeyboardButton("📝 Вікторини"), KeyboardButton("🌐 Спільнота")],
-        [KeyboardButton("Профіль"), KeyboardButton("🔍 Пошук")]
+        [KeyboardButton("Профіль"), KeyboardButton("🔍 Пошук"), KeyboardButton("Мета на сьогодні")]
     ]
     return ReplyKeyboardMarkup(buttons, resize_keyboard=True)
 
@@ -26,6 +28,9 @@ async def main_menu_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) 
     elif user_input == "Профіль":
         await profile_handler(update, context)
         return States.PROFILE_MENU
+    elif user_input == "Мета на сьогодні":
+        await send_tier_list(update, context)
+        return States.MAIN_MENU
     else:
         await update.message.reply_text("Ця функція наразі недоступна. Оберіть іншу опцію.")
         return States.MAIN_MENU
