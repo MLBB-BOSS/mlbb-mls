fb7daa77fab1da612b3a8b1b67a9d81bf1eb12c8
Олег <74383033+Oleg513@users.noreply.github.com>
2024-11-05 07:49:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,43 +5,18 @@ from telegram.ext import (
     CommandHandler,
     MessageHandler,
     ConversationHandler,
-    filters,
-    ContextTypes
+    filters
 )
 from config.settings import TELEGRAM_BOT_TOKEN
 from handlers.states import States
 from handlers.start_handler import start
+from handlers.main_menu import main_menu_handler
 from handlers.characters import (
-    handle_characters_menu,
     handle_selecting_hero_class,
     handle_selecting_hero,
-    handle_hero_functions_menu,
-    handle_comparison_first_hero,
-    handle_comparison_second_hero,
-    handle_selecting_counter_hero
+    handle_hero_functions_menu
 )
-# Видаляємо імпорти неіснуючих модулів
-# from handlers.main_menu import main_menu_handler
-# from handlers.guides import handle_guides_menu
-# from handlers.tournaments import handle_tournaments_menu
-# from handlers.updates import handle_updates_menu
-# from handlers.beginner import handle_beginner_menu
-# from handlers.news import handle_news_menu
-# from handlers.help_menu import handle_help_menu
-# from handlers.quizzes import (
-#     handle_quizzes_menu,
-#     handle_guess_the_hero
-# )
-# from handlers.search import (
-#     handle_search_menu,
-#     handle_search_performing,
-#     handle_search_hero_guides
-# )
-# from handlers.comparisons import handle_comparisons_menu
-# from handlers.emblems import handle_emblems_menu
-# from handlers.items import handle_items_menu
-# from handlers.recommendations import handle_recommendations
-# from handlers.trigger_handler import trigger_handler
+# Видаляємо зайві імпорти
 
 # Налаштування логування
 logging.basicConfig(
@@ -60,26 +35,15 @@ def main():
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
         states={
-            States.MAIN_MENU: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_characters_menu)],
-            States.CHARACTERS_MENU: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_characters_menu)],
-            States.SELECTING_HERO_CLASS: [
-                MessageHandler(filters.TEXT & ~filters.COMMAND, handle_selecting_hero_class)
-            ],
-            States.SELECTING_HERO: [
-                MessageHandler(filters.TEXT & ~filters.COMMAND, handle_selecting_hero)
-            ],
+            States.MAIN_MENU: [MessageHandler(filters.TEXT & ~filters.COMMAND, main_menu_handler)],
+            States.SELECTING_HERO_CLASS: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_selecting_hero_class)],
+            States.SELECTING_HERO: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_selecting_hero)],
             States.HERO_FUNCTIONS_MENU: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_hero_functions_menu)],
-            States.COMPARISON_FIRST_HERO: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_comparison_first_hero)],
-            States.COMPARISON_SECOND_HERO: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_comparison_second_hero)],
-            States.SELECTING_COUNTER_HERO: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_selecting_counter_hero)],
-            # Видаляємо стани, для яких немає обробників
-            # States.GUIDES_MENU: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_guides_menu)],
             # Додайте інші стани за потребою
         },
         fallbacks=[
             CommandHandler('start', start),
-            # Видаляємо trigger_handler, якщо його немає
-            # MessageHandler(filters.TEXT & ~filters.COMMAND, trigger_handler)
+            # Можна додати обробник для невідомих команд
         ]
     )
     application.add_handler(conv_handler)
