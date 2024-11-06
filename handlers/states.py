b2b322ae28b15821c81060aa71950f77a6be2aef
Олег <74383033+Oleg513@users.noreply.github.com>
2024-11-06 03:34:48 +0200

    Update states.py

--- handlers/states.py
+++ handlers/states.py
@@ -1,10 +1,11 @@
 # handlers/states.py
+
 from enum import Enum
 
 class States(Enum):
-    MAIN_MENU = 0
-    SELECTING_HERO_CLASS = 1
-    SELECTING_HERO = 2
-    HERO_FUNCTIONS_MENU = 3
-    PROFILE_MENU = 4
+    MAIN_MENU = 1
+    SELECTING_HERO_CLASS = 2
+    SELECTING_HERO = 3
+    HERO_FUNCTIONS_MENU = 4
+    PROFILE_MENU = 5
     # Додайте інші стани за потреби
